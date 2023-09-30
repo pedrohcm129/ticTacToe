32fccafe0e7860d33da6e245e5da95188e68922c
Pedro Henrique Costa Menzes <pedrohcm729@gmail.com>
2023-09-30 13:46:06 -0300

    first commit

---
+++ player.py
@@ -0,0 +1,23 @@
+class Player:
+    def __init__(self, letter):
+        self.letter
+
+
+    def get_move(self):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+
+    def get_move(self):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+
+    def get_move(self):
+        pass
